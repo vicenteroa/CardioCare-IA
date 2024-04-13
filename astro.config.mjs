cf1a216f8bb5931ab0c065be4411a6e9c272f928
Vicente Roa <vicenteroa03@gmail.com>
2024-04-13 15:12:47 -0400

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: vercel(),
   integrations: [react(), tailwind()]
-});
+})
