fd16d33fcae0cf0bcb3469a7e8ab3ff38633dfae
Vicente Roa <vicenteroa03@gmail.com>
2024-04-13 15:19:36 -0400

    Update 3

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -2,7 +2,11 @@ import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async ({ redirect, cookies }) => {
   cookies.delete('session', {
-    path: '/'
+    path: '/',
+    domain: undefined,
+    httpOnly: undefined,
+    sameSite: undefined,
+    secure: undefined
   })
   return redirect('/signin')
 }
