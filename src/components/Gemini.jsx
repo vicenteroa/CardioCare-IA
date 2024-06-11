9345515095d2aec3c61e5e3b8634ed3eea21b295
Vicente Roa <vicenteroa03@gmail.com>
2024-06-11 15:23:34 -0400

    Integrations API: Vercel

--- src/components/Gemini.jsx
+++ src/components/Gemini.jsx
@@ -1,10 +1,8 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
-const key =
-  import.meta.env.MODE === 'development'
-    ? import.meta.env.PUBLIC_APIKEYGEMINI_DEVELOPMENT
-    : import.meta.env.APIKEYGEMINI_PRODUCTION
+
+const key = import.meta.env.PUBLIC_APIKEYGEMINI
 
 const AiWithText = () => {
   const [search, setSearch] = useState('')
@@ -12,7 +10,7 @@ const AiWithText = () => {
   const [loading, setLoading] = useState(false)
 
   const generativeAI = new GoogleGenerativeAI(key)
-  console.log('key', key)
+  // console.log('key', key)
   const generativeModel = generativeAI.getGenerativeModel({ model: 'gemini-pro' })
 
   const fetchTextInsights = async (userInput) => {
